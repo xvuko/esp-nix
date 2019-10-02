ec1d1ef47ca52a0b8b8b8f186c7ba539186c0f1f
vuko <vuko@hackerspace.pl>
2019-10-02 23:35:37 +0200

    change espressif toolkint version to 1.5.4

--- esp-open-sdk.nix
+++ esp-open-sdk.nix
@@ -1,12 +1,19 @@
 { pkgs ? import <nixpkgs> {} }:
 
 let
+  python = pkgs.python2Full.withPackages (python-packages: with python-packages; [
+    pyserial
+  ]); 
   crosstool_ng = import ./crostool-ng.nix {};
   sdk = pkgs.fetchFromGitHub {
     owner = "espressif";
     repo = "ESP8266_NONOS_SDK";
     rev = "61248df5f6";
     sha256 = "175y9v0lpp1f89a1sghschs131r5wmqisphvdzr3y8yrqmx2dljs";
+  };
+  sdk154 = pkgs.fetchurl {
+    url = "http://bbs.espressif.com/download/file.php?id=1469";
+    sha256 = "01pkshzm7a45v4sp8281nypsi0fmqcy07fr8ncbbg43rv55cf88h";
   };
 in pkgs.stdenvNoCC.mkDerivation {
   name = "esp-open-sdk";
@@ -30,16 +37,20 @@ in pkgs.stdenvNoCC.mkDerivation {
     cp -vr ${crosstool_ng} ./xtensa-lx106-elf/
     chmod -R u+w ./xtensa-lx106-elf
 
-    cp -vr ${sdk} ESP8266_NONOS_SDK-2.1.0-18-g61248df
-    chmod -R u+w ESP8266_NONOS_SDK-2.1.0-18-g61248df
+    #cp -vr ${sdk} ESP8266_NONOS_SDK-2.1.0-18-g61248df
+    #chmod -R u+w ESP8266_NONOS_SDK-2.1.0-18-g61248df
 
-    ls ./xtensa-lx106-elf/bin/
+    cp -r ${sdk154} ESP8266_NONOS_SDK_V1.5.4_16_05_20.zip
+  '';
+  buildPhase = ''
+    make VENDOR_SDK=1.5.4 STANDALONE=y
   '';
   installPhase = ''
     mkdir -p $out
-    cp -avr ESP8266_NONOS_SDK-2.1.0-18-g61248df $out/sdk
+    cp -avr $(readlink -e sdk) $out/sdk
     cp -avr xtensa-lx106-elf $out/
+    cp -avr esptool $out/
   '';
-  buildInputs = with pkgs; [ which gnumake autoconf automake libtool gperf flex bison texinfo gawk ncurses.dev expat python3 python3Packages.pyserial gnused git unzip bash help2man bzip2 binutils ];
+  buildInputs = with pkgs; [ which gnumake autoconf automake libtool gperf flex bison texinfo gawk ncurses.dev expat python gnused git unzip bash help2man bzip2 binutils ];
   hardeningDisable = [ "format" ];
 }
