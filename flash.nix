24e3202d7d445a1fb27916434acd1e2f1b97a6c3
vuko <vuko@hackerspace.pl>
2021-11-01 09:31:39 +0100

    update flashing command

--- flash.nix
+++ flash.nix
@@ -3,9 +3,11 @@
 
 let 
   python = pkgs.python3.withPackages (pp: with pp; [ pyserial ]);
-in pkgs.writeScriptBin "flash" ''
+in pkgs.writeScriptBin "flash" 
+  # @begin=python@
+  ''
     #!${pkgs.python3}/bin/python
-
+    
     import argparse
     import subprocess
     from pathlib import Path
@@ -17,18 +19,26 @@ in pkgs.writeScriptBin "flash" ''
     python = '${python}/bin/python'
     esptool = '${sming}/Sming/Components/esptool/esptool/esptool.py'
     fw = Path(__file__).parent.parent.joinpath('firmware').absolute()
-
+    
     address = "0x100000"
     
     fargs = [
         '-p', args.serial,
         '-b', '115200',
+        '--chip', 'esp8266',
+        '--before', 'default_reset',
+        '--after', 'hard_reset' 
     ]
-
+    
     iargs = [
+      '-z', '-fs', '1MB', '-ff', '40m', '-fm', 'dio',
       '0x000000', fw.joinpath("rboot.bin"),
       '0x002000', fw.joinpath("rom0.bin"),
+      '0x000fa000', fw.joinpath('partitions.bin'),
+      '0x000fc000', '${sming}/Sming/Arch/Esp8266/Components/esp8266/ESP8266_NONOS_SDK/bin/esp_init_data_default.bin'
     ]
-      
+    
+    
     subprocess.run([python, esptool, *fargs, 'write_flash', *iargs ])
 ''
+  # @end=python@
